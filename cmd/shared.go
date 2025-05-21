eaab22672e5317bb93ab44afd3ad42b43622fe3b
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2025-05-21 13:28:52 -0500

    Remove `backend` commands
    These don't work due to the removal of `/actuator/info`.

--- cmd/shared.go
+++ cmd/shared.go
@@ -7,7 +7,6 @@ import (
 func CreateDataDirs() {
 	utils.CreateDirectory("data")
 	utils.CreateDirectory("data/helm")
-	utils.CreateDirectory("data/actuator")
 	utils.CreateDirectory("data/deployments")
 	utils.CreateDirectory("data/pods")
 	utils.CreateDirectory("data/secrets")
